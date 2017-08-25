aed312c4256fd3b70a4ac20f8c3aa6a30c5a5beb
RBrons <rbronsguru89@outlook.com>
2017-08-25 23:18:30 +0600

    registration with each fields

--- client/views/register/register.js
+++ client/views/register/register.js
@@ -27,19 +27,26 @@ Template.Register.events({
 
 		var submit_button = $(t.find(":submit"));
 
+		// credentials
 		var register_username = t.find('#register_username').value.trim();
-		var register_email = t.find('#register_email').value.trim();
 		var register_password = t.find('#register_password').value;
 		var confirm_password = t.find('#confirm_password').value;
-		var register_name = t.find('#register_name').value.trim();
+		// personal info
+		var register_email = t.find('#register_email').value.trim();
+		var register_first_name = t.find('#register_first_name').value.trim();
+		var register_last_name = t.find('#register_last_name').value.trim();
+		var register_cell_no = t.find('#register_cell_no').value.trim();
+		// instagram info
+		var register_instagram = t.find('#register_instagram').value.trim();
+		var register_insta_pass = t.find('#register_insta_pass').value.trim();
+		var confirm_insta_pass = t.find('#confirm_insta_pass').value.trim();
+		// targets
+		var register_target_audience = t.find('#register_target_audience').value.trim();
+		var register_target_hashtags = t.find('#register_target_hashtags').value.trim();
+		var register_phrase = t.find('#register_phrase').value.trim();
+		var register_yes_no = t.find('#register_yes_no').value.trim();
+		var register_country = t.find('#register_country').value.trim();
 
-		// check name
-		if(register_name == "")
-		{
-			pageSession.set("errorMessage", "Please enter your name.");
-			t.find('#register_name').focus();
-			return false;
-		}
 
 		// check email
 		if(!isValidEmail(register_email))
@@ -59,20 +66,44 @@ Template.Register.events({
 			return false;
 		}
 
-		if(!isValidPassword(register_password, min_password_len))
+		if(!isValidPassword(register_password, min_password_len)) // TODO make it strong
 		{
 			pageSession.set("errorMessage", "Your password must be at least " + min_password_len + " characters long.");
 			t.find('#register_password').focus();
 			return false;
 		}
 
+		// check instagram pass
+		if(register_insta_pass !== confirm_insta_pass)
+		{
+			pageSession.set("errorMessage", "Instagram passwords don't match.");
+			t.find('#confirm_insta_pass').focus();
+			return false;
+		}
+
+		// creating user
 		submit_button.button("loading");
 		Accounts.createUser({
 			username: register_username,
 			email: register_email,
 			password : register_password,
 			profile: {
-				name: register_name
+				// personal
+				firstName: register_first_name,
+				lastName: register_last_name,
+				cellPhone: register_cell_no,
+				// targets
+				targetAudience: register_target_audience,
+				targetHashtag: register_target_hashtags,
+				commentPhrase: register_phrase,
+				yesNo: register_yes_no,
+				country: register_country,
+				// instagram info
+				instagram: [{
+					inUsername: register_instagram,
+					accountPass: register_insta_pass,
+					confirmPass: confirm_insta_pass
+				}]
 			}
 		}, function(err) {
 			submit_button.button("reset");
