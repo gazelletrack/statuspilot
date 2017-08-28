7fb474e230d38fd1abc6538db323e76fc2e3d86c
RBrons <rbronsguru89@outlook.com>
2017-08-29 02:47:40 +0600

    instagram account template
    TO DO: Add more accounts and udpate functionalities

---
+++ client/views/user_settings/instagram_accounts/instagrams.js
@@ -0,0 +1,55 @@
+var pageSession = new ReactiveDict();
+
+pageSession.set("errorMessage", "");
+pageSession.set("infoMessage", "");
+
+Template.UserSettingsInstagrams.onCreated(function() {
+	pageSession.set("errorMessage", "");	
+	pageSession.set("infoMessage", "");	
+	
+});
+
+Template.UserSettingsInstagrams.onDestroyed(function() {
+	
+});
+
+Template.UserSettingsInstagrams.onRendered(function() {
+
+
+	Meteor.defer(function() {
+		globalOnRendered();
+		$("input[autofocus]").focus();
+	});
+});
+
+Template.UserSettingsInstagrams.events({
+	'submit #instagrams_form' : function(e, t) {
+		e.preventDefault();
+
+		pageSession.set("errorMessage", "");
+		pageSession.set("infoMessage", "");
+
+		var submit_button = $(t.find(":submit"));
+
+		var password = t.find('#password').value;
+
+		submit_button.button("loading");
+
+		return false; 
+	}
+
+});
+
+Template.UserSettingsInstagrams.helpers({
+	errorMessage: function() {
+		return pageSession.get("errorMessage");
+	},
+	infoMessage: function() {
+		return pageSession.get("infoMessage");
+	},
+	InstagramAccounts: function() {
+		console.log(Users.findOne({_id:Meteor.userId()}, {}).profile.instagram);
+		return Users.findOne({_id:Meteor.userId()}, {}).profile.instagram;
+	}
+	
+});
